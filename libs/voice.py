406903db2f99f84767627c73147272426291ab43
mikeurbi97 <aurbina24397@outlook.com>
2017-05-25 02:50:32 -0500

    Now telling rememberings

--- libs/voice.py
+++ libs/voice.py
@@ -4,35 +4,14 @@ import speech_recognition as sr
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 import sys, os, datetime, pyaudio, time
-import pymysql as psql
+import MySQLdb as mysql
 
-def escuchar():
-	playsound("beep.mp3")
-	r = sr.Recognizer()
-	m = sr.Microphone()
-	with m as src:
-		print("Di un comando")
-		audio = r.listen(src)
-
-	try:
-		speech = rec.recognize_google(audio)
-		command = speech.split(' ')
-		action(command)
-	except sr.UnknownValueError:
-		talk('No te he entendido, intenta nuevamente.')
-		escuchar()
-	except sr.RequestError as e:
-		print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-#def action(command):
-#	return {
-#		'dime' : say()
-#		'di' : say()
-#		'crear' : newRemember()
-#	}[command]
+# id_user de prueba, obtener mediante conexion con Andromeda mobile
+id_user = '1'
 
 modeldir = "C:\Python27\Lib\site-packages\pocketsphinx\model"
 datadir = "C:\Python27\Lib\site-packages\pocketsphinx\data"
+
 def continuousSpeech():
 	print("Di \"Andromeda\" para empezar")
 	# Create a decoder with certain model
@@ -63,12 +42,51 @@ def continuousSpeech():
 	        stream.stop_stream()
 	        stream.close()
 	        p.terminate()
-	        escuchar()
+	        activo=1
+	        break
+	if activo==1:
+		escuchar()
+	
 
+def escuchar():
+	playsound("beep.mp3")
+	r = sr.Recognizer()
+	m = sr.Microphone()
+	with m as src:
+		print("Di un comando")
+		audio = r.listen(src)
+
+	try:
+		speech = r.recognize_google(audio, language='es-US')
+		print(speech)
+		action(speech)
+	except sr.UnknownValueError:
+		talk('No te he entendido, intenta nuevamente.', "error")
+		escuchar()
+	except sr.RequestError as e:
+		print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+def action(command):
+	if command.find('Di') != -1:
+		say(command)
+	elif command.find('Dime') != -1:
+		say(command)
+	elif command.find('dime') != -1:
+		say(command)
+	elif command.find('di') != -1:
+		say(command)
+
+def say(command):
+	if command.find('recordatorios de hoy') != -1:
+		print("say")
+		dia = time.strftime("%Y-%m-%d")
+		select_recordatorios_dia(id_user, dia)
+	elif command.find('todos') != -1:
+		select_todos_recordatorios(id_user)
 
 def conectar():
 	global conn
-	conn = psql.connect(host='localhost', port=3306, user='root', passwd='', db='andromeda')
+	conn = mysql.connect(host='localhost', port=3306, user='root', passwd='', db='andromeda')
 	global cur 
 	cur = conn.cursor()
 	print(conn)
@@ -78,21 +96,33 @@ def terminar(conn, cur):
 	conn.close()
 
 def select_recordatorios_dia(id_user, dia):
-	cur.execute("SELECT descripcion FROM andromeda_recordatorios WHERE idAndromedaUser_id = '"+id_user+"' AND diaRecordatorio BETWEEN '"+dia+" 00:00:00' AND '"+dia+" 23:59:59' AND idEstadoRecordatorio_id = 1")
+	cur.execute("SELECT descripcion FROM andromeda_recordatorios WHERE idAndromedaUser_id = "+id_user+" AND diaRecordatorio BETWEEN '"+dia+" 00:00:00.000000' AND '"+dia+" 23:59:59.999999' AND idEstadoRecordatorio_id = 1")
+	data = cur.fetchall()
+	i=0
+	talk("Hoy tienes que", "hoy")
+	for row in data:
+		print(row[0])
+		talk(row[0], "select"+str(i))
+		i = i+1
 
-def nuevo_recordatorio(descripcion, dia, tipo, id_user):
-	cur.execute("INSERT INTO andromeda_recordatorios VALUES(0, '"+descripcion+"', '"+dia+"', '"+str(datetime.now())+"', '"+id_user+"', 1, '"+tipo+"'")
+def select_todos_recordatorios(id_user):
+	cur.execute("SELECT descripcion FROM andromeda_recordatorios WHERE idAndromedaUser_id = '"+id_user+"' AND idEstadoRecordatorio_id = 1")
+	data = cur.fetchall()
+	i=0
+	talk("Tus recordatorios son", "all")
+	for row in data:
+		print(row[0])
+		talk(row[0], "all"+str(i))
+		i = i+1
 
-def cambiar_estado_recordatorio(estado, id_user, id_recordatorio):
-	cur.execute("UPDATE andromeda_recordatorios SET idEstadoRecordatorio_id = '"+estado+"' WHERE idRecordatorio = '"+id_recordatorio+"'")
-
-def talk(text):
+def talk(text, filename):
 	tts = gTTS(text=text, lang='es-us')
-	file = 'sample.mp3'
+	file = filename+'.mp3'
 	tts.save(file)
-	playsound('sample.mp3')
+	playsound(file)
+	os.remove(file)
 
 conectar()
 continuousSpeech()
-terminar()
+terminar(conn, cur)
 
